c9cc7d44aa52460544f362e727f4ec119c6a347b
Dimitar Danailov <dimitar.danailov@mentormate.com>
2016-02-16 14:52:13 +0200

    Added a new route for pages and added a new request for getting a page list.

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import App from './components/App';
 const {Route} = Router;
 
 const routes = <Route handler={App}>
+	<Route name='page' path='/page/:id' />
 </Route>;
 
 Router.run(routes, Router.HistoryLocation, Root => 
--- src/components/PageList.js
+++ src/components/PageList.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import * as API from '../api';
 
+import {Link} from 'react-router';
+
 export default class PageList extends React.Component {
 	state = {
+		'loaded': false,
+		'pages': {},
 		'newPageTitle': ''
 	}
 
+	componentDidMount() {
+		API.pages.on('value', records => this.setState({
+			'pages': records.exportVal() || [],
+			'loaded': true
+		}));
+	}
+
 	render() {
+		let items = this.state.loaded ? this.pageListItems() : [<li key='loading'> <em> Loading .. </em> </li> ];
+
 		return (
 			<section>
+				<ul> { items } </ul>
 				{
 					this.props.user ? this.renderComponent() : null
 				}
@@ -16,22 +30,35 @@ export default class PageList extends React.Component {
 		);
 	}
 
+	/**
+	 * Create input field for adding a new page.
+	 */
 	renderComponent() {
 		return (
 			<input type='text'
 						 className='u-full-width'
 						 placeholder='New Page Title'
-						 onKeyPress={this.createPage}
+						 value={this.state.newPageTitle}
+						 onKeyPress={this.createPage} 
 						 onChange={this.updateTitle} />
 		);
+	}
+
+	/**
+	 * Create a list with page information
+	 */
+	pageListItems() {
+		let items = Object.keys(this.state.pages).map(id => <li key={id}>
+				<Link to='page' params={ { id: id } }>{this.state.pages[id].title}</Link>
+			</li>);
+
+		return items;
 	}
 
 	updateTitle = evt => this.setState({ 'newPageTitle': evt.target.value });
 	createPage = evt => { 
 		// We looking for pressing for space key 
 		if (evt.charCode !== 13) return;
-
-		console.log('Title: ' + this.state.newPageTitle);
 
 		API.pages.push({ 'title': this.state.newPageTitle });
 		this.setState({ 'newPageTitle': '' });
